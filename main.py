7b2b835722e5559c1ed5127c195ab034ff1692b4
garry-nn <102152292+garry-nn@users.noreply.github.com>
2022-03-27 15:59:23 +0300

    Update main.py
    Someone, may be I, make changes in my prog ...

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('I do it every day')
-
+    print('Again')
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
